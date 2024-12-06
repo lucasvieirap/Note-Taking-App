cb0b156c38f909b25fc3f066691ba39fee6b96b0
lucsvp <lucasvieirapalavro@gmail.com>
2024-12-06 15:28:57 -0300

    refactor: add MarshalJSON to note
    This allows enconding of JSON to the Model

--- internal/models/note.go
+++ internal/models/note.go
@@ -1,8 +1,19 @@
 package models
 
+import (
+	"strconv"
+)
+
 type Note struct {
 	ID	 uint    `json:"id"`
 	Markdown []byte  `json:"note"`
+}
+
+func (n Note) MarshalJSON() ([]byte, error) {
+	id := `"` + strconv.Itoa(int(n.ID)) + `"`
+	markdown := `"` + string(n.Markdown) + `"`
+
+	return []byte(`{` + `"id":` + id + `,` + `"note":` + markdown + `}`), nil
 }
 
 // TODO: Note Validation
